daa278d6f52aaa2eedba200a7e0c07da643400ca
Thales Carvalho <thalessd@live.com>
2020-02-29 17:31:04 -0300

    Remoçao de comentário

--- make_identifier/helpers.py
+++ make_identifier/helpers.py
@@ -11,7 +11,6 @@ def random_hex_uppercase(string_length=10):
 
 def resource_path(relative_path):
     try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
