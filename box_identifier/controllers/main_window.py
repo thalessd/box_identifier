ae1f2c77cb691a3eba8a3b950208d84d099432fc
Thales Carvalho <thalessd@live.com>
2020-03-02 11:14:59 -0300

    Finalização da Janela inicial

--- box_identifier/controllers/main_window.py
+++ box_identifier/controllers/main_window.py
@@ -9,12 +9,17 @@ from box_identifier.scripts import constants
 def run():
     app = QApplication(sys.argv)
 
+    app.setStyle('Fusion')
+
     file = QFile(path.join(constants.VIEWS_FOLDER, "mainwindow.ui"))
     file.open(QFile.ReadOnly)
 
     loader = QUiLoader()
 
     window = loader.load(file)
+
+    window.setFixedSize(600, 450)
+
     window.show()
 
     sys.exit(app.exec_())
